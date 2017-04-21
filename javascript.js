ea7675adc9226ec510befaf2c0afc97eefaf6555
lai-joyce <lai.joyce@gmail.com>
2017-04-20 19:37:01 -0700

    index2.html

---
+++ javascript.js
@@ -0,0 +1,25 @@
+document.getElementById("button1").addEventListener("click", function(){
+	document.getElementById("box").style.height = "250px";
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+	document.getElementById("box").style.background = "blue";
+});
+
+document.getElementById("button5").addEventListener("click", function(){
+	document.getElementById("box").style.opacity = "0.5";
+});
+
+document.getElementById("button4").addEventListener("click", function() {
+	document.getElementById("box").style.height = "150px";
+	document.getElementById("box").style.background = "orange";
+	document.getElementById("box").style.opacity = "1.0";
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+	document.getElementById("box").style.transition = "all 0.5s";
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+	document.getElementById("box").style.background = "white";
+});
